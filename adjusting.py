1e8885e913a518e7a85fb418b03580a9bb24245d
Laoge Fu <hongming_fu@brown.edu>
2024-05-10 05:24:16 -0400

    '8th_try'

--- adjusting.py
+++
@@ -1,7 +0,0 @@
-# Open the file with the assumed incorrect encoding and read the contents
-with open('output.txt', 'r', encoding='windows-1252') as file:
-    text = file.read()
-
-# Write the contents back with the correct UTF-8 encoding
-with open('output.txt', 'w', encoding='utf-8') as file:
-    file.write(text)
